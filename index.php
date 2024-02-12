95ec57ac88cb5ff77969d73f222f4faaa351f384
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-12 16:16:42 -0300

    Add style

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php 
-  echo "hello world"
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primeiro exemplo PHP</title>
+
+  <style>
+    h2{
+      color: #80a2ff;
+      text-shadow: 1px 1px 1px #222;
+    }
+  </style>
+
+</head>
+
+<body>
+  <?php 
+    echo "<h2>Hello World!</h2>";
+  ?>
+
+</body>
+</html>
+
+
