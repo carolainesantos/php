f26cce82dad6e4835035c3604061ca3443dbbbb8
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-13 18:38:53 -0300

    Terminando div valores pra pegar na url

--- aula03.php
+++ aula03.php
@@ -16,7 +16,7 @@
       </h3>
     </div>
 
-    <!-- Seção: Variáveis em PHP -->
+    <!-- Seção: Operações Aritméticas em PHP -->
     <h4>Operações Aritméticas em PHP</h4>
     <?= " Uma coisa importante é saber a ordem de precedência de operadores aritméticos em PHP. Sempre em uma expressão, os operadores que serão executados são, na ordem:<br><br>" ?>
 
@@ -24,11 +24,16 @@
     <?= "• Em segundo lugar, operadores de multiplicação, divisão e módulo * / %<br>" ?>
     <?= "• Em seguida, as adições e subtrações + - <br>" ?>
 
-    <!-- Seção: Comandos de saída do PHP -->
+    <!-- Seção: Obtendo valores da URL com PHP -->
     <h4>Obtendo valores da URL com PHP</h4>
-    <?= "<br><br>" ?>
-    <?= "echo 'Estou aprendendo variáveis em PHP';<br><br>" ?>
-    <?= "Foi utilizada uma tag HTML para a quebra de linha. Isso é possível, já que o PHP se integra com essa tecnologia.<br><br>" ?>
+    <?= " Exemplo de url abaixo:<br>" ?>
+    <?= "http://localhost/aula05/operadores.php?a=3&b=2<br><br>" ?>
+    <?= "No link acima, o arquivo operadores.php está sendo chamado dentro da pasta aula05 do servidor local. Na linha, serão passados dois valores: a valendo 3 e b valendo 2.<br><br>" ?>
+    <?= "Para pegar esses valores no script PHP, usar o código:<br>" ?>
+    <pre>
+      <?= htmlspecialchars('$valor1 = $_GET["a"];') ?><br>
+      <?= htmlspecialchars('$valor2 = $_GET["b"];') ?>
+    </pre>
 
     <!-- Seção: Exemplo de Código PHP -->
     <div>
