9deea8374e2928a588a96d9b0d86a6ed97cc4f74
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-16 13:04:50 -0300

    Adicionando espaçamentos

--- aula03.php
+++ aula03.php
@@ -18,7 +18,7 @@
     </div>
     
     <!-- Seção: Operações Aritméticas em PHP -->
-    <h4>Operações Aritméticas em PHP</h4>
+    <h4>Operações Aritméticas em PHP:</h4>
     <?= " Uma coisa importante é saber a ordem de precedência de operadores aritméticos em PHP. <br>Sempre em uma expressão, os operadores que serão executados são, na ordem:<br><br>" ?>
     
     <?= "• Em primeiro lugar, parênteses ( ).<br>" ?>
@@ -56,24 +56,45 @@
     echo "<br/>Saída → O modulo vale: ". ($n1 % $n2);
     echo "<br/>Saída → A média vale: ". $media;
     ?>
+
+<h4>Obtendo valores da URL com PHP:</h4>
+        <?= " Exemplo de url abaixo:<br>" ?>
+        <?= "http://localhost/aula05/operadores.php?a=3&b=2<br><br>" ?>
   </div> <!-- Fecha div container-->
   
   <div class="card02">
     <div class="Conteudo">
-      <h4>Obtendo valores da URL com PHP</h4>
-        <?= " Exemplo de url abaixo:<br>" ?>
-        <?= "http://localhost/aula05/operadores.php?a=3&b=2<br><br>" ?>
-        <?= "No link acima, o arquivo operadores.php está sendo chamado dentro da pasta <br>aula05 do servidor local. Na linha, serão passados dois valores: a valendo 3 e b valendo 2.<br><br>" ?>
-        <?= "Para pegar esses valores no script PHP, usar o código:<br>" ?>
+      
+        <?= "No link anterior, o arquivo operadores.php está sendo chamado dentro da pasta <br>aula05 do servidor local. Na linha, serão passados dois valores:<br> 'a' valendo 3 e 'b' valendo 2.<br><br>" ?>
+        <?= "Para pegar esses valores no script PHP, deve usar o código:<br>" ?>
           <pre>
             <?= htmlspecialchars('$valor1 = $_GET["a"];') ?><br>
             <?= htmlspecialchars('$valor2 = $_GET["b"];') ?>
           </pre>
-              <p>No código anterior, o parâmetro a (3) passado pela URL será armazenado na variável $valor1.</p>
-              <p>De maneira similar, a variável $valor2 vai conter o valor do parâmetro b (2).<br>
+          <p>No código anterior, o parâmetro a (3) passado pela URL será armazenado na variável $valor1.</p>
+          <p>De maneira similar, a variável $valor2 vai conter o valor do parâmetro b (2).<br>
               Obs: Usar sempre $_GET com todas as letras maiusculas.</p>
-              <?= "Funções Matemáticas em PHP abs() : Retorna o valor absoluto de um número." ?><br>
-              <?= "Por exemplo: abs(-5) = 5" ?>
+
+          <h4>Funções Matemáticas em PHP:</h4>
+
+          <p>abs() : Retorna o valor absoluto de um número.<br>
+              Exemplo: abs(-5) = 5</p>
+
+          <p>pow(): Calcula uma potência<br>
+              exemplo: pow(3,2) = 32 = 9</p>
+
+          <p>sqrt(): Calcula a raiz quadrada de um número.<br>
+              exemplo: sqrt(25) = 5</p>
+
+          <p>round(): Arredonda valores.<br>
+              exemplo: round(3.8) = 4</p>
+
+          <p>intval() : Trunca um número.<br>
+              Retorna a parte inteira de um valor.<br>
+              exemplo: abs(8.7) = 8</p>
+
+          <p>number_format() : Formata um número Real.<br>
+              exemplo: number_format(3258.754, 2, “,”, “.”) = 3.258. 75.</p>
         </div>
       </div>
   </body>
