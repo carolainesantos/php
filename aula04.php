adc0d4a15b19a6fb3a2b2415ee75f65e8b7b8d10
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-20 11:46:08 -0300

    Adicionando exemplos

--- aula04.php
+++ aula04.php
@@ -23,7 +23,12 @@
     $v1 = $_GET["x"];
     $v2 = $_GET["y"];
     echo "<h4>Valores recebidos na url: $v1 e $v2</h4>";
-    echo "O valor absoluto de $v2 e " . abs($v2);
+    echo "O valor absoluto de $v2 é = " . abs($v2);
+    echo "<br>A potencia do valor de $v1<sup>$v2</sup>  é = " . pow($v1, $v2);
+    echo "<br>A raiz de $v1 é = " . sqrt($v1);
+    echo "<br>O valor de $v2 arredondado é = " . round($v2); // ceil=sempre arredonda pra cima ou floor= sempre pra baixo
+    echo "<br>A parte inteira de $v2 é = " . intval($v2); // trunca numeros 
+    echo "<br>O valor de $v1 em moeda é = R$" . number_format($v1, 2,",", ".");
   ?>
 </body>
 </html>
