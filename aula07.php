f7116449efca4b05a6281d4284fdd6f89c37d114
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-26 15:17:42 -0300

    Criando arquivo css aula 07

---
+++ aula07.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Estudadndo PHP</title>
+  <link rel="stylesheet" href="./css/style07.css">
+</head>
+<body>
+<div class="card-container">
+    <div class="card01">
+      <div class="title">
+        <h3>
+          <?php
+        echo "Aula 07";
+        ?>
+      </h3>
+    </div>
+
+   <!-- Este exemplo -->
+  <?php 
+    echo "<h4>Operadores Relacionais</h4>";
+  ?>
+
+  <!-- -->
+
+  <?php 
+   
+  ?>
+
+   <!-- 
+    -->
+  <?php 
+
+  ?>
+</body>
+</html>
