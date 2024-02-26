662c0b84ae9bb5dc98393ec825b8b8b8d4550717
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-26 16:45:59 -0300

    Adicionando estilo

--- aula07.php
+++ aula07.php
@@ -42,7 +42,10 @@
 
    <!--  -->
   <?php 
-    
+    $nota1 = $_GET["n1"];
+    $nota2 = $_GET["n2"];
+    $media = ($nota1 + $nota2) / 2;
+    echo "A media entre $nota1 e $nota2 é $media";
   ?>
 </body>
 </html>
