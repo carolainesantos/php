7e0a02483df25c14960f5c85203403b7e5cd24c0
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-28 14:21:44 -0300

    Adicionando inputs

--- 01valor.php
+++ 01valor.php
@@ -20,7 +20,9 @@
 
   <?php 
     $valor = $_GET["b"];
-    echo "O valor enviado foi $valor";
+    $rq = sqrt($valor);
+    echo "A raiz de  $valor é igual a: ". number_format($rq, 2);
   ?>
+  <a href="01exerc.html">Voltar</a>
 </body>
 </html>
