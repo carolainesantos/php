63cc4ef5717d23936819367f279fe5176a2cd90f
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-28 14:11:23 -0300

    Adicionando formulario

---
+++ 01valor.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Estudadndo PHP</title>
+  <link rel="stylesheet" href="./css/style09.css">
+  
+</head>
+<body>
+<div class="card-container">
+    <div class="card01">
+      <div class="title">
+        <h3>
+          <?php
+        echo "Aula 09";
+        ?>
+      </h3>
+    </div>
+
+  <?php 
+    $valor = $_GET["b"];
+    echo "O valor enviado foi $valor";
+  ?>
+</body>
+</html>
