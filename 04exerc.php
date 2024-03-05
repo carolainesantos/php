61f168f37d101f4b2018486e75947f3847fa5536
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-03-05 14:43:57 -0300

    Exercicio 04 Finalizado

--- 04exerc.php
+++ 04exerc.php
@@ -19,10 +19,33 @@
     </div>
 
   <?php 
-    echo "<h4>Integração HTML5 + PHP</h4>";
-    echo "<span class='texto'>$txt</span>";
-  ?>
+     echo "<h4>Integração HTML5 + PHP</h4>";
+     $a = isset($_GET["ano"])?$_GET["ano"]:1900;
+     $i = date("Y") - $a;
+     echo "<h4>Você nasceu no ano: $a e tem $i anos.</h4>";
 
+    /* exemplo 01
+      if($i >=18) {
+       $v = "ja pode votar";
+       $d = "ja pode dirigir";
+     } 
+     else {
+      $v = "não pode votar";
+      $d = "não pode dirigir";
+    }  
+    echo "Com essa idade você, $v e também $d. <br/>"; */
+
+    if($i < 16) {
+      $tipoVoto = "não vota";
+    } 
+    elseif (($i >= 16 && $i < 18) || ($i > 65)) {
+      $tipoVoto = "voto opcional";
+    }
+    else {
+      $tipoVoto = "voto obrigatório";
+    }
+    echo "Para essa idade, $tipoVoto. <br/>";
+  ?>
   <a href="04exerc.html">Voltar a Page HTML</a>
 
 </body>
