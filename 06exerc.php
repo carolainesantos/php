f56b768912b1749338ae54aa0485c5a8feb6df7f
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-03-11 16:36:49 -0300

    Exercicio 06 finalizado

--- 06exerc.php
+++ 06exerc.php
@@ -19,20 +19,27 @@
     </div>
 
   <?php 
-    echo "<h4>Integração HTML5 + PHP</h4>";
-    $n1 = $_GET["nota1"];
-    $n2 = $_GET["nota2"];
-    $media = ($n1 + $n2) / 2;
-
-      if($media >= 7) {
-        echo "APROVADO";
-      } 
-      elseif ($media < 7 && $media >= 5) {
-        echo "RECUPERAÇÃO";
-      }
-      else {
-        echo "REPROVADO";
-      }
+    echo "<h4>Switch PHP</h4>";
+    $n = isset($_GET["num"]) ?$_GET["num"] : 0;
+    $o = isset($_GET["oper"]) ?$_GET["oper"] : 1;
+    switch($o){
+      case 1:
+        $r = $n * 2;
+        echo "O dobro de $n é $r";
+        break;
+      case 2:
+        $r = $n ^ 3;
+        echo "O cubo de $n é $r";
+        break;
+      case 3:
+        $r = sqrt($n);
+        echo "A raiz quadrada de $n é $r";
+        break;
+      default:
+        echo "Opção inválida";
+        break;
+    }
+    echo "<br/>O resultado da operação solicitada foi <span class='foco'>$r</span>";
   ?>
 
   <a href= "06exerc.html" >Voltar a Page HTML</a>
